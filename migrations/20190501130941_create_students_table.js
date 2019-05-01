9298cb651a3d436a5a05d4b510f210dd4675ba8f
AmarachiOE <ao.ejiawoko@gmail.com>
2019-05-01 13:19:04 -0700

    Created migrations for cohorts and students tables in lambda DB

---
+++ migrations/20190501130941_create_students_table.js
@@ -0,0 +1,19 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("students", tbl => {
+    tbl.increments("student_id");
+
+    tbl.string("name", 255).notNullable();
+    tbl
+      .integer("cohort_id")
+      .unsigned()
+      .references("cohort_id")
+      .inTable("cohort")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    tbl.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("students");
+};
