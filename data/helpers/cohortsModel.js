a3ef00679f0c82bf5c66d0db146658a168c50372
AmarachiOE <ao.ejiawoko@gmail.com>
2019-05-01 14:07:48 -0700

    GET, GET by id, GET students by id requests complete for /api/cohorts, /api/cohorts/:id, and /api/cohorts/:id/students endpoints

--- data/helpers/cohortsModel.js
+++ data/helpers/cohortsModel.js
@@ -1,13 +1,41 @@
 const db = require("../dbConfig");
 
 module.exports = {
-    find,
-    // findById,
-    // insert,
-    // update,
-    // remove
-  };
+  find,
+  findById,
+  findStudentsById,
+  insert,
+  update,
+  remove
+};
 
-  function find() {
-    return db("cohorts");
-  }
+// ==== NOTE on Cohorts table:  cohorts_id
+// ==== NOTE on Students table: cohort_id
+
+function find() {
+  return db("cohorts");
+}
+
+function findById(id) {
+  return db("cohorts")
+    .where({ cohorts_id: Number(id) }) // id field on table is 'cohorts_id' not 'id'
+    .first();
+}
+
+function findStudentsById(id) {
+  return db("cohorts as c")
+    .join("students as s", "s.cohort_id", "c.cohorts_id") // cohort_id AND cohorts_id
+    .select("c.cohorts_id", "c.name", "s.name as StudentName")
+    .where({ cohorts_id: Number(id) });
+}
+
+function insert() {
+  return db("cohorts");
+}
+
+function update() {
+  return db("cohorts");
+}
+function remove() {
+  return db("cohorts");
+}
--- routers/cohorts-router.js
+++ routers/cohorts-router.js
@@ -1,18 +1,56 @@
 const cohortsRouter = require("express").Router();
 const cohortsDb = require("../data/helpers/cohortsModel.js");
 
-
 // ============ GET ALL
 cohortsRouter.get("/", (req, res) => {
-    cohortsDb.find().then(cohorts => {
-        res.status(200).json(cohorts);
-    }).catch(err => {
-        res
-          .status(500)
-          .json({ error: "The cohorts information could not be retrieved." });
-      });
+  cohortsDb
+    .find()
+    .then(cohorts => {
+      res.status(200).json(cohorts);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The cohorts information could not be retrieved." });
+    });
 });
 
+// ============ GET BY ID
+cohortsRouter.get("/:id", (req, res) => {
+  const cohortId = req.params.id;
+  cohortsDb
+    .findById(cohortId)
+    .then(cohort => {
+      if (cohort) {
+        res.status(200).json(cohort);
+      } else {
+        res.status(404).json({ error: "This cohort could not be found." });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "This cohort's information could not be retrieved." });
+    });
+});
 
+// ============ GET STUDENTS BY COHORT ID
+cohortsRouter.get("/:id/students", (req, res) => {
+  const cohortId = req.params.id;
+  cohortsDb
+    .findStudentsById(cohortId)
+    .then(cohort => {
+      if (cohort) {
+        res.status(200).json(cohort);
+      } else {
+        res.status(404).json({ error: "This cohort could not be found." });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "This cohort's information could not be retrieved." });
+    });
+});
 
 module.exports = cohortsRouter;
